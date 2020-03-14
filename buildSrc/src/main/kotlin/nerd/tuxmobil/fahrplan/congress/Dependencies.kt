6605847ad4315adc14cdeae519ac654cca6b5516
Tobias Preuss <tobias.preuss@googlemail.com>
2020-03-14 11:36:15 +0100

    DEBUGGING -> Serve data via local webserver. CUSTOMIZE IP ADDRESS!
    + Example webserver: python3 -m http.server

--- buildSrc/src/main/kotlin/nerd/tuxmobil/fahrplan/congress/Dependencies.kt
+++ buildSrc/src/main/kotlin/nerd/tuxmobil/fahrplan/congress/Dependencies.kt
@@ -6,7 +6,7 @@ object Android {
     const val buildToolsVersion = "29.0.3"
     const val compileSdkVersion = 29
     const val minSdkVersion = 14
-    const val targetSdkVersion = 29
+    const val targetSdkVersion = 27
 }
 
 private const val kotlinVersion = "1.3.72"
